789c1e2d94fab3d293a799e10a4e91da1b9442c6
Aritra50 <99112478+Aritra50@users.noreply.github.com>
2022-02-06 11:44:28 +0530

    Update String_Session.py

--- String_Session.py
+++ String_Session.py
@@ -11,8 +11,8 @@ print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = int(input("10569448: "))
+API_HASH = input("1dbc26fdca78d41c7cb5e564883e2a3b: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
